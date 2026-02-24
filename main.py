bde9b6162ab5b8ecbb211f9d4b31235a78fed25d
Valentin OUVRARD <valentin@ops.nc>
2026-02-25 10:04:28 +1100

    1er commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def root():
+    return "<h1>Hello World 😀</h1>"
+
+@app.get("/ping")
+def ping():
+     return {"pong": True}
+
